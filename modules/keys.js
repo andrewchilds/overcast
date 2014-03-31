8176194671c2d5cc67f43c15a2ee3fd5f62c9792
Andrew Childs <ac@glomerate.com>
2014-03-31 01:05:36 -0400

    AC: Create .overcast/keys directory

--- modules/keys.js
+++ modules/keys.js
@@ -10,7 +10,7 @@ exports.notFound = function () {
 
 exports.create = function (done) {
   var keyfile = utils.CONFIG_DIR + '/keys/overcast.key';
-  exec('ssh-keygen -t rsa -N "" -f ' + keyfile + ' && chmod 600 ' + keyfile, function (err) {
+  exec('mkdir -p ' + utils.CONFIG_DIR + '/keys && ssh-keygen -t rsa -N "" -f ' + keyfile + ' && chmod 600 ' + keyfile, function (err) {
     if (err) {
       console.log('Error generating SSH key.');
       console.log(err);
