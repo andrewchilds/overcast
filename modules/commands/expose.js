d9ccf04f2b6bad7b3be13c27f3d27979f18be609
Andrew Childs <ac@glomerate.com>
2014-04-10 15:59:24 -0400

    AC: Standardize sed usage in scripts, add IP whitelisting to expose command

--- modules/commands/expose.js
+++ modules/commands/expose.js
@@ -17,13 +17,16 @@ exports.run = function (args) {
     exposed_ports: args._.join(' ')
   };
   args._ = ['install/iptables'];
+  if (args.whitelist) {
+    args.env.ip_whitelist = args.whitelist;
+  }
 
   ssh(args);
 };
 
 exports.signatures = function () {
   return [
-    '  overcast expose [instance|cluster|all] [port...]'
+    '  overcast expose [instance|cluster|all] [port...] [options]'
   ];
 };
 
@@ -32,11 +35,17 @@ exports.help = function (args) {
     'overcast expose [instance|cluster|all] [port...]',
     '  Reset the exposed ports on the instance or cluster using iptables.'.grey,
     '  This will fail if you don\'t include the current SSH port.'.grey,
-    '  Expects an Ubuntu server, untested on other distributions.'.yellow,
+    '  Specifying --whitelist will restrict all ports to the specified address(es).'.grey,
+    '  These can be individual IPs or CIDR ranges, such as "192.168.0.0/24".',
+    '',
+    '  Expects an Ubuntu server, untested on other distributions.'.grey,
+    '',
+    '    Option'.grey,
+    '    --whitelist "IP IP IP"'.grey,
     '',
     '  Examples:'.grey,
     '  $ overcast expose redis 22 6379'.grey,
-    '  $ overcast expose mysql 22 3306'.grey,
+    '  $ overcast expose mysql 22 3306 --whitelist "1.1.1.1 2.2.2.2 192.168.100.0/24"'.grey,
     '  $ overcast expose app 22 80 443'.grey
   ]);
 };
