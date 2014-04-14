9f005a8cba4bfb217053bed81a99742c8e79b4fa
Andrew Childs <ac@glomerate.com>
2014-04-14 10:25:02 -0400

    AC: Missing linebreak escape

--- modules/utils.js
+++ modules/utils.js
@@ -4,7 +4,7 @@ var _ = require('lodash');
 var colors = require('colors');
 var list = require('./commands/list');
 
-exports.VERSION = '0.1.26';
+exports.VERSION = '0.1.27';
 
 exports.clustersCache = null;
 exports.variablesCache = null;
