936b5e8109d3e3261682f1c02379eaee3afa345d
Andrew Childs <ac@glomerate.com>
2014-12-04 16:23:48 -0500

    Refactor health/init commands

--- modules/commands/init.js
+++ modules/commands/init.js
@@ -2,26 +2,22 @@ var fs = require('fs');
 var _ = require('lodash');
 var utils = require('../utils');
 
-exports.run = function (args) {
-  var cwd = process.cwd();
+var commands = {};
+exports.commands = commands;
 
-  if (fs.existsSync(cwd + '/.overcast')) {
-    utils.alert('.overcast directory already exists here.');
-  } else {
-    utils.initOvercastDir(cwd);
+commands.init = {
+  usage: 'overcast init',
+  description: [
+    'Create an .overcast config directory in the current working directory.',
+    'No action taken if one already exists.'
+  ],
+  run: function (args) {
+    var cwd = process.cwd();
+
+    if (fs.existsSync(cwd + '/.overcast')) {
+      utils.alert('.overcast directory already exists here.');
+    } else {
+      utils.initOvercastDir(cwd);
+    }
   }
-};
-
-exports.signatures = function () {
-  return [
-    '  overcast init'
-  ];
-};
-
-exports.help = function () {
-  utils.printArray([
-    'overcast init',
-    '  Create an .overcast config directory in the current working directory.'.grey,
-    '  No action taken if one already exists.'.grey
-  ]);
 };
