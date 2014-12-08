7199865f3ca8211b87e922e37540c0fe56fb2a88
Andrew Childs <ac@glomerate.com>
2014-12-07 22:56:30 -0500

    Refactor import and remove commands

--- modules/commands/remove.js
+++ modules/commands/remove.js
@@ -1,25 +1,18 @@
 var utils = require('../utils');
-var instanceCommand = require('./instance');
+var instance = require('./instance');
 
-exports.run = function (args) {
-  args.command = 'instance';
-  args._.unshift('remove');
-  instanceCommand.run(args);
-};
+var commands = {};
+exports.commands = commands;
 
-exports.signatures = function () {
-  return [
-    '  overcast remove [name]'
-  ];
-};
-
-exports.help = function () {
-  utils.printArray([
-    'overcast remove [name]',
-    '  Removes an instance from the index.'.grey,
-    '  The server itself is not affected by this action.'.grey,
-    '',
-    '  Example:'.grey,
-    '  $ overcast remove app.01'.grey
-  ]);
+commands.remove = {
+  name: 'remove',
+  usage: 'overcast remove [name]',
+  description: instance.commands.remove.description,
+  examples: instance.commands.remove.examples,
+  required: instance.commands.remove.required,
+  run: function (args) {
+    args.command = 'instance';
+    args._.unshift('remove');
+    instance.commands.remove.run(args);
+  }
 };
--- modules/filters.js
+++ modules/filters.js
@@ -1,7 +1,7 @@
 var utils = require('./utils');
 
 exports.findMatchingInstances = function (name, args) {
-  args.instances = utils.findMatchingInstancesByInstanceName(name);
+  args.instances = utils.findMatchingInstances(name);
 
   if (args.instances.length === 0) {
     utils.dieWithList('No instances found matching "' + name + '".');
